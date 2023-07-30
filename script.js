ee980aeec4a6a9b59c2e889147eef666e0f0f9e5
bishtntl <131008556+bishtntl@users.noreply.github.com>
2023-07-30 21:40:23 +0530

    Add files via upload
    ok

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 let task=document.getElementById("popupbox")
-const container=document.getElementById("container")
+let container=document.getElementById("container")
 let cardinput =document.getElementById("userinput")
 let popupboxnew=document.getElementById("popupboxnew")
-
+let globalcontainer=document.getElementsByClassName("global")[0]
+let copycontainer=document.getElementById("copycontainer")
+let globalcontainer2=document.getElementsByClassName("globalhero")
+let revertbutton=document.getElementById("back")
+let count=0;
+let noitem=document.querySelectorAll(".noitem")
+let blank=document.getElementById("blank")
+let head=document.getElementById("head")
+let listone=document.getElementById("list")
 //  ***************************************************************************************************//
 let box =false
-let box2=false
+//let box2=false
 //  function of when we click on  clickitem button
 function addpopup(){
    
+   count++;
+   if(count>=1){
+    noitem[0].remove();
+    head.remove()
+   listone.remove()
+   
+
+   }
+
     if(box){
         //add class here and thair display none in css card is not visible its pre define in css
         task.classList.add("hide");
@@ -18,35 +35,36 @@ function addpopup(){
     // and poppup card is visible
     else{
         task.classList.remove("hide");
-        box=true;  
+        box=true;
+       
+       
+       
     }
-    // document.body.style.backgroundColor="white"
-document.body.style.opacity=0.8,
-   task.style.opacity=1
+    globalcontainer.classList.add("addblur")
+   
 
+  
 }
 
-function addpopupnew(){
-    if(box2){
-    popupboxnew.classList.add("hide")
-    box2=false    
-}
-else{
-    popupboxnew.classList.remove("hide")
-box2=true
-}
-}
+
 
 // second close button function
 function hideme(){
     task.classList.add("hide");
     box=false;
-}
+    globalcontainer.classList.remove("addblur")
 
+}
+hideme();
 // ********************card container**************
 
+
+
 // creating a structure of card add heading inside div and 2 button in the on div //
+
+
 function addcard(){
+    
     //creating element//
     let newcard =document.createElement("div")
     let heading =document.createElement("h2")
@@ -57,7 +75,7 @@ function addcard(){
 
 
 
-
+  
 //*****************************************************************************************************//
 
 
@@ -83,6 +101,68 @@ function addcard(){
  removebutton.setAttribute("class","btn4")
 hrline.setAttribute("class","line")
 heading.setAttribute("class","high")
+newdiv.setAttribute('class','itemstore')
+
+
+
+
+
+heading.addEventListener('click',function(){
+
+    globalcontainer.style.display="none"
+         container.style.display="none"
+        // container.classList.add("hideg")
+        globalcontainer2[0].style.display="block"
+        //  copycontainer.style.display="block"
+        copycontainer.appendChild(newcard)
+        // newcard.appendChild(heading)
+
+        copycontainer.style.display="flex"
+        blank.innerText=heading.innerText
+        blank.style.color="white"
+    
+
+    
+  
+    
+
+
+    copycontainer.classList.add("hide2")
+
+
+
+       
+       
+
+
+})
+revertbutton.addEventListener('click',()=>{
+    showback()
+})
+function showback(){
+
+     
+        container.style.display="flex"
+        copycontainer.style.display="none"
+        copycontainer.innerHTML=" "
+              
+         globalcontainer2[0].style.display="none"
+         container.appendChild(newcard) 
+         globalcontainer.style.display="block"
+        
+
+     }
+      
+
+
+
+
+// newcard.style.height="auto"
+ removebutton.addEventListener('click',()=>{
+    // hideme();
+
+
+ })
 
 
 // **************************************************************************data of card
@@ -92,116 +172,99 @@ addbutton.innerHTML="+"
 removebutton.innerHTML="-"
 cardinput.value="";
 
-
-
-
+ hideme();
 
 addbutton.addEventListener("click",()=>{
-addpopupnew();
+let body=document.querySelector("body")
+let popupbox2=document.createElement("div")
+let newtitle=document.createElement("h2")
+let newinput=document.createElement("input")
+let newbuttonadd=document.createElement("button")
+let newclosebutton=document.createElement("button")
+
+
+// /appendChild here>>>>>>>>>>>>>>>>>
+
+body.appendChild(popupbox2);
+popupbox2.appendChild(newtitle);
+popupbox2.appendChild(newinput);
+popupbox2.appendChild(newbuttonadd);
+popupbox2.appendChild(newclosebutton);
+
+
+
+//adding class and id here.......>>>>>>>>>>
+
+popupbox2.setAttribute("class",'popupboxnew')
+newinput.setAttribute("class","userinput2")
+newbuttonadd.setAttribute("id","btnaddone")
+newclosebutton.setAttribute("id","btnremoveone")
+
+
+
+
+// /adding inner html
+
+newtitle.innerHTML="ADD NEW TASK"
+newbuttonadd.innerHTML="Add"
+newclosebutton.innerHTML="close"
+
+
+
+newclosebutton.addEventListener("click",()=>{popupbox2.remove()})
+
+newbuttonadd.addEventListener("click",()=>{
+ 
+
+    // newcard.style.height="auto"
+    // let newdiv = document.createElement("div")
+  
+  let addtitle= document.createElement("span")
+  let lastbtn=  document.createElement("button")
+
+
+  addtitle.innerHTML=newinput.value
+
+
+  popupbox2.classList.add("hide")
+  newclosebutton.classList.add("hide")
+  
+
+
+
+    newdiv.appendChild(addtitle)
+    newdiv.appendChild(lastbtn)
+
+
+    newdiv.setAttribute('class','itemstore')
+    lastbtn.classList.add("btnlinethrough")
+    addtitle.classList.add("showitem")
+
+    lastbtn.innerHTML="MarkDone"
+    // lastbtn.style.display="inlineblock"
+    // addtitle.style.display="inlineblock"
+
+
+    
+    lastbtn.addEventListener('click',()=>{
+        // addtitle.classList.add("showitem")
+        addtitle.classList.add("linethrough")
+        lastbtn.style.display="none"
+        
+    
+    })
+})
 
 })
-    hideme()
+
 }
-// function popup2(){
-//     document.createElement("h3")
-//     document.createElement("button")
-//     document.
+  
 
 
-//     newdiv.innerHTML=newinputvalue.value
 
 
 
 
-// }
-
-
-
-
-   
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let addnewitem = document.createElement("div");
-// let title = document.createElement("h2")
-// let input = document.createElement("input")
-// let taskaddbutton = document.createElement("button")
-// let taskremovebutton = document.createElement("button")
-
-
-
-// body.appendChild(addnewitem);
-
-// addnewitem.appendChild(title);
-
-// addnewitem.appendChild(input);
-
-// addnewitem.appendChild(taskaddbutton);
-
-// addnewitem.appendChild(taskremovebutton);
-
-
-
-
-
-// addnewitem.setAttribute(`class`,"popupboxnew")
-
-// title.setAttribute(`class`,"title")
-
-// input.setAttribute(`class`,"newinputvalue")
-
-// taskaddbutton.setAttribute(`class`,"taskbutton1")
-
-// taskremovebutton.setAttribute(`class`,"taskbutton1")
-
-
-
-// // newdiv.innerHTML= input .value
-
-
-
-
-
-
-   
-// })
-
-
-// }
-
-// function addbutton(){
-
-// }
 
 
 
